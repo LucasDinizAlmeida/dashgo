d7755b687437ec6454845039dfae386c0cc1de8e
Lucas Diniz Almeida <almeidalucas820@gmail.com>
2022-08-22 23:13:01 -0300

    Integrating mirageJS into the application for Fake API consumption.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,13 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { AppProps } from 'next/app.js'
 import { SidebarDrawerContextProvider } from '../contexts/SidebarDrawerContext'
+import { startMakeServer } from '../services/mirage'
 import { theme } from '../styles/theme'
+
+if (process.env.NODE_ENV === 'development') {
+  startMakeServer()
+}
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
---
+++ src/services/mirage/index.ts
@@ -0,0 +1,31 @@
+import { createServer, Model } from 'miragejs'
+
+type User = {
+  name: string,
+  email: string,
+  create_at: string
+}
+
+export function startMakeServer() {
+
+  const server = createServer({
+
+    models: {
+      user: Model.extend<Partial<User>>({})
+    },
+
+    routes() {
+      this.namespace = 'api'
+      this.timing = 750
+
+      this.get('/users')
+      this.post('/users')
+
+      this.namespace = ''
+
+      this.passthrough()
+    }
+  })
+
+  return server
+}
